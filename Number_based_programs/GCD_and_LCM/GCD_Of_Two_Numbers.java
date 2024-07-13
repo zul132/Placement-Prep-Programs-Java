5c82a263834d8a0ffec5dbae62ee14c2cb0e41e4
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-07-13 13:46:41 +0530

    Added GCD programs

--- Number_based_programs/GCD_and_LCM/GCD_Of_Two_Numbers.java
+++ Number_based_programs/GCD_and_LCM/GCD_Of_Two_Numbers.java
@@ -1,28 +1,27 @@
+
 //Non-recursive method to find GCD of two numbers
 import java.util.Scanner;
 
 class GCD_Of_Two_Numbers {
-    public static int GCD(int a, int b)
-    {
-        if (a < b){
+    static int GCD(int a, int b) {
+        if (a < b) {
             int t = a;
             a = b;
             b = t;
         }
         int r;
-        while (b != 0)
-        {
+        while (b != 0) {
             r = a % b;
             a = b;
             b = r;
         }
         return a;
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        System.out.println(GCD(A,B));
+        System.out.println(GCD(A, B));
     }
 }
