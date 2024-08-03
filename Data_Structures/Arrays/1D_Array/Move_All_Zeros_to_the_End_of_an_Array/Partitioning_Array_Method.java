a6eb74b31a3729dc25fdc821f2ec2441101a63e6
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-08-03 19:50:14 +0530

    Added Partitioning the Array Solution for Pushing All Zeros to the End of Array problem

--- Data_Structures/Arrays/1D_Array/Move_All_Zeros_to_the_End_of_an_Array/Partitioning_Array_Method.java
+++ Data_Structures/Arrays/1D_Array/Move_All_Zeros_to_the_End_of_an_Array/Partitioning_Array_Method.java
@@ -1,46 +1,43 @@
 /* Given an array of random numbers, Push all the zero’s of the given array to the end of the array. 
- * Asked in Multicoreware, SAP Labs interviews.
+ * APPROACH 2: Partioning the Array
 */
 //Time Complexity: O(n) where n is the number of elements of the input array.
 //Auziliary Space: O(1)
 
 import java.util.Scanner;
 
-public class Move_All_Zeros_To_End_Array {
+public class Partitioning_Array_Method {
     static void pushZerosToEnd(int arr[], int n) {
-        int count = 0;
+        int j = 0; // Pivot element
 
-        // Traverse the array. If element encountered is non-
-        // zero, then replace the element at index 'count'
-        // with this element and increment 'count'.
         for (int i = 0; i < n; i++) {
             if (arr[i] != 0) {
-                arr[count] = arr[i];
-                count += 1;
+                swap(arr, j, i); // Partion the array
+                j++;
             }
         }
+    }
 
-        // Now all non-zero elements have been shifted to
-        // front and 'count' is set as index of first 0.
-        // Make all elements 0 from count to end.
-        while (count < n) {
-            arr[count++] = 0;
-        }
+    static void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // size of array
 
+        // Input the array elements
         int[] arr = new int[n];
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
         pushZerosToEnd(arr, n);
+
         // print the array after pushing all zeros to the end
-        System.out.println("Array after pushing all zeros to the end");
+        System.out.println("Array after pushing all zeros to the end:");
         for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
     }
-
 }
