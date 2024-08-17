4340cea4693b4eedce6a82920eb075d8be4b7508
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-08-17 22:36:09 +0530

    Added string anagram program

--- String_programs/Strings_Set1/Anagram.java
+++ String_programs/Strings_Set1/Anagram.java
@@ -1,35 +1,52 @@
+/*  
+    Write a program to check whether the given strings are anagram or not. An anagram is a word or phrase formed by rearranging the letters
+    of a different word or phrase, typically using all the original letters exactly once.
+
+    Input Format:
+    INTEGRAL
+    TRIANGLE
+    Output Format:
+    It is an anagram
+
+    Sample Input 0:
+    xyzab
+    axyzh
+    Sample Output 0:
+    It is not an anagram
+*/
 import java.util.Scanner;
 
-class Anagram 
-{
-    static boolean isAnagram(String a, String b) {
-        if (a.length() != b.length()) {
+public class Anagram {
+    static boolean isAnagram(String s1, String s2) {
+        // Check if the lengths of the two strings are equal
+        if (s1.length() != s2.length())
             return false;
-        } else {
-            int[] hash = new int[26];
-            for (int i = 0; i < a.length(); i++) {
-				hash[a.charAt(i) - 97]++;
-                hash[b.charAt(i) - 97]--;
+        else { 
+            int hash[] = new int[26];
+            for (int i = 0; i < s1.length(); i++) {
+                hash[s1.charAt(i) - 97]++;
+                hash[s2.charAt(i) - 97]--;
             }
             for (int i = 0; i < 26; i++)
                 if (hash[i] != 0)
                     return false;
+            return true;
         }
-        return true;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s1 = sc.next();
-        String s2 = sc.next();
-		
-		s1 = s1.toLowerCase();
-		s2 = s2.toLowerCase();
+        String input1 = sc.next();
+        String input2 = sc.next();
 
-        if (isAnagram(s1, s2))
-            System.out.println("Anagram");
+        // Convert the input strings to Lowercase
+        input1 = input1.toLowerCase();
+        input2 = input2.toLowerCase();
+
+        if (isAnagram(input1, input2))
+            System.out.println("It is an anagram");
         else
-            System.out.println("Not an Anagram");
+            System.out.println("It is not an anagram");
         sc.close();
     }
 }
