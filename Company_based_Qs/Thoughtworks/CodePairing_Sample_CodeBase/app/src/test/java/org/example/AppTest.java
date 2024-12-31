b26791c68fdf9941a86ba6e180a7645afb810581
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-12-31 18:52:07 +0530

    created classes for the codebase

--- Company_based_Qs/Thoughtworks/CodePairing_Sample_CodeBase/app/src/test/java/org/example/AppTest.java
+++ Company_based_Qs/Thoughtworks/CodePairing_Sample_CodeBase/app/src/test/java/org/example/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        ECommerceApp classUnderTest = new ECommerceApp();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
